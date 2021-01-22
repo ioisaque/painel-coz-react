88df59e48d049ff26fa14483b755a38859efb840
Isaque Costa <isaquecostaa@gmail.com>
2021-01-22 06:55:40 -0300

    url fix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://beta.lmsalgados.com.br/webservices",
-  baseURL: "https://localhost/lmsalgados.com.br/webservices",
+  baseURL: "http://localhost/lmsalgados.com.br/webservices",
 });
 
 export default api;
